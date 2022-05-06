60aa8872d2e85897497eab1513babc1328d6148c
Isaac Sexe <isaac.sexe@gmail.com>
2022-05-05 19:23:56 -0500

    changed installer

--- scripts/install/easy/easy_installer.sh
+++ scripts/install/easy/easy_installer.sh
@@ -6,6 +6,6 @@ scripts/install/easy/git_setup.sh
 echo "...Finished"; echo "";
 
 echo "Checking dependencies..."
-scripts/install/easy/dependency_installer.sh
+scripts/install/easy/dependency_installer.sh || exit 1;
 # ./dependency_installer.sh
 echo "...Finished"; echo "";
